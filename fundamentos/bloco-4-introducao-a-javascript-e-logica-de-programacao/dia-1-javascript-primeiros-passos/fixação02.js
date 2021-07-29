63605b5573f48d4f21b030b8de4a3eff4bfc5786
Daniel Dias <danieldias.b99@hotmail.com>
2021-07-29 02:53:07 -0300

    Exercício fixação 02.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/fixação02.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/fixação02.js
@@ -1,7 +1,13 @@
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
+//
+//Crie uma variável chamada base e uma variável chamada altura e atribua os seus respectivos valores: 5 e 8;
+//Crie uma variável chamada area e atribua a ela o resultado da multiplicação da base pela altura. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+//Crie uma variável chamada perimetro e atribua a ela a soma de todos os lados do retângulo;
+let base = 5;
+let altura = 6;
+let area = base * altura;
+let perimetro = base * 2 + altura * 2;
+
+console.log("Base:" + base);
+console.log("Altura:" + altura);
+console.log("Área:" + area);
+console.log("Perímetro:" + perimetro);
