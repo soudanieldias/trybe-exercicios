ae3aa7a7ae33bd6672cc3f58cd7fa835d6fc25b7
Daniel Dias <danieldias.b99@hotmail.com>
2021-07-29 20:28:22 -0300

    Envia exercicio06.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio06.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio06.js
@@ -3,5 +3,39 @@
 //  Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case) .
 //  Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
 //  Exemplo: bishop (bispo) -> diagonals (diagonais)
+/*
+REFERÊNCIA UTILIZADA: https://www.ludijogos.com/multiplayer/xadrez/regras/
+*/
 
+let xadrezPiece = "bispo";
 
+switch (xadrezPiece.toLowerCase())
+{
+    case 'rei':
+        console.log("Rei ->> Todas direções, uma casa por vez.");
+        break;
+
+    case 'rainha':
+        console.log("Rainha ->> Move-se em todas as direções, mas não pula outras peças.");
+        break;
+
+    case 'bispo':
+        console.log("Bispo ->> Move-se na diagonal, mas não pula outras peças.");
+        break;
+
+    case 'cavalo':
+        console.log("Cavalo ->> Movimenta-se em L, para qualquer direção.");
+        break;
+
+    case 'torre':
+        console.log("Torre ->> Move-se na Vertical/Horizontal, mas não pula outras peças.");
+        break;
+
+    case 'peão':
+        console.log("Peão ->> Move-se uma casa a frente, e apenas captura peças em sua diagonal");
+        break;
+
+    default:
+        console.log("PEÇA INVÁLIDA!!!");
+        break;
+}
