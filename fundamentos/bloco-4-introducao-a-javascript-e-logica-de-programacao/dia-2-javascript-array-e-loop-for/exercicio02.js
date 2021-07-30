73996fc3fe54d99008d8a1a90abb364ecff4e54f
Daniel Dias <danieldias.b99@hotmail.com>
2021-07-30 03:51:13 -0300

    Envia exercicio02.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio02.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio02.js
@@ -1,3 +1,10 @@
 //Para o segundo exercício, some todos os valores contidos no array e imprima o resultado; 
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (number of numbers)
+{
+    soma += number;
+}
+console.log("Soma = " + soma);
