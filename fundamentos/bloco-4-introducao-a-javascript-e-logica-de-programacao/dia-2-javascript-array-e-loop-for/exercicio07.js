4a28eb22aeeb2f6d0ea7cce9cf76b32b78263c45
Daniel Dias <danieldias.b99@hotmail.com>
2021-07-30 16:15:37 -0300

    Envia exercicio07.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio07.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio07.js
@@ -1,3 +1,12 @@
 //Utilizando for , descubra qual o menor valor contido no array e imprima-o; 
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+var min = numbers.reduce(
+    function(a, b)
+    {
+        return Math.min(a, b);
+    }
+);
+
+console.log("Menor termo: " + min);
