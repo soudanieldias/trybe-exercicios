d68f85cedf6a8201ab59c9cc61e61017dded05e6
Daniel Dias <danieldias.b99@hotmail.com>
2021-07-30 16:32:50 -0300

    Envia exercicio09.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio09.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio09.js
@@ -1,3 +1,13 @@
 //Utilizando o array criado no exercício anterior imprima o resultado da divisão
 //de cada um dos elementos por 2 . 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+
+//Para o termo na posição 'number' dentro do array 'numbers'
+//Faça ...
+for (number in numbers)
+{
+    //Divisão de <VALOR> por 2: <VALOR/2>
+    divisao = numbers[number]/2;
+    console.log("Divisão de " + numbers[number] + "por 2: " + divisao );
+}
