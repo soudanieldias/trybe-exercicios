d9b3bbbf2a6087a44de3a447331de1ce520e9e00
Daniel Dias <danieldias.b99@hotmail.com>
2021-07-30 04:09:10 -0300

    Envia exercicio05.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio05.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio05.js
@@ -1,3 +1,12 @@
 //Utilizando for , descubra qual o maior valor contido no array e imprima-o; 
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+/*var max = Math.max(numbers);
+*/
+
+var max = numbers.reduce(function(a, b) {
+  return Math.max(a, b);
+});
+
+console.log("Maior termo: " + max);
