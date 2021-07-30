f154eb170dcb392d4e650983db984e9b823ed528
Daniel Dias <danieldias.b99@hotmail.com>
2021-07-30 15:35:45 -0300

    Envia exercicio06.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio05.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio05.js
@@ -5,8 +5,11 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 /*var max = Math.max(numbers);
 */
 
-var max = numbers.reduce(function(a, b) {
-  return Math.max(a, b);
-});
+var max = numbers.reduce(
+    function(a, b)
+    {
+        return Math.max(a, b);
+    }
+);
 
 console.log("Maior termo: " + max);
--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio06.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio06.js
@@ -2,3 +2,14 @@
 //Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (number of numbers)
+{
+    if ( (number%2) != 0 )
+    {
+        console.log("Número ímpar Encontrado: " + number);
+    } else
+    {
+        console.log("Número par Encontrado: " + number);
+    }
+}
