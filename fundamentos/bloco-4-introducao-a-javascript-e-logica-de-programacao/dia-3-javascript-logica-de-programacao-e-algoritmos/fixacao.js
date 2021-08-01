77799a976e70d8dcfc6f8977ab8e94f47d038d84
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-01 16:47:44 -0300

    Soluciona fixacaoo01.js

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/fixacao.js
@@ -0,0 +1,15 @@
+const n = 9;
+
+let resultado = 100;
+for (let i = 0; i <= n; i += 1)
+{
+    resultado -= i;
+}
+console.log(resultado);const n = 9;
+
+let resultado = 100;
+for (let i = 0; i <= n; i += 1)
+{
+    resultado -= i;
+}
+console.log(resultado);
