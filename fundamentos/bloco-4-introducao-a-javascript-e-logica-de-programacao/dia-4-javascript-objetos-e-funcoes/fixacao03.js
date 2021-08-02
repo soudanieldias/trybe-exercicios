4f889f112c2f108c4ad7581224e54b88607dc684
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-02 15:16:40 -0300

    Finaliza fixacao03.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/fixacao03.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/fixacao03.js
@@ -8,3 +8,7 @@ let player = {
     age: 34,
     medals: { golden: 2, silver: 3 }
 };
+
+console.log("A jogadora " + player.name + " " + player.lastName + " Possui " + player.age + " anos de idade.");
+
+player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
