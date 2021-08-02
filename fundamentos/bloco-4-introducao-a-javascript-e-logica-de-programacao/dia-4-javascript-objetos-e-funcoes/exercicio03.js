6ac89fa8da5c730a02c5fe1352a1d5f3b6f0acf9
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-02 16:24:21 -0300

    Adiciona indice dos exercicios.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio03.js
@@ -0,0 +1,16 @@
+// Usando o objeto abaixo, faça os exercícios a seguir:
+
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
+/*****************************************************************************/
+/*
+ Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+
+ personagem
+ origem
+ nota
+ recorrente
+*/
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio05.js
@@ -0,0 +1,16 @@
+// Usando o objeto abaixo, faça os exercícios a seguir:
+
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
+/*****************************************************************************/
+/*
+Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+
+    Margarida e Tio Patinhas
+    Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+    Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+    Ambos recorrentes // Atenção para essa última linha!
+*/
