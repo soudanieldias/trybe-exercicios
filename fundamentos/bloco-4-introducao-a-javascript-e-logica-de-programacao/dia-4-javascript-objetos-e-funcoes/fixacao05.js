55b9d0dee94537192296b289b4fd477d0324c3f9
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-02 15:21:43 -0300

    Finaliza exercicio05.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/fixacao05.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/fixacao05.js
@@ -7,3 +7,5 @@ let player = {
     age: 34,
     medals: { golden: 2, silver: 3 }
 };
+
+console.log("A jogadora possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " medalhas de prata");
