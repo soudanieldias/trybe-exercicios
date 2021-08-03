91c4cba8b909d077af8fb7a7ef7ecd2613cc067c
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-03 02:11:08 -0300

    Finaliza exercicio11.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio11.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio11.js
@@ -4,3 +4,14 @@ Crie uma função que receba um número inteiro N e retorne o somatório de todo
 Valor de teste: N = 5 .
 Valor esperado no retorno da função: 1+2+3+4+5 = 15 .
 */
+
+function somaNumeros(numeros)
+{
+  let soma = 0;
+  for (let index = 1; index <= numeros; index++)
+  {
+    soma += index;
+  }
+  return soma;
+}
+console.log( somaNumeros(5) ); //15
