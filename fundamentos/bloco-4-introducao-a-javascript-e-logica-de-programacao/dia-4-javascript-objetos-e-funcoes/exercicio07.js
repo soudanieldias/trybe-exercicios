efecc60edb153c6477b2e6554c8fb08c431ff2f7
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-02 22:18:39 -0300

    Finaliza exercicio07.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio07.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio07.js
@@ -4,3 +4,30 @@ Crie uma função que receba um array de inteiros e retorne o índice do maior v
     Array de teste: [2, 3, 6, 7, 10, 1]; .
     Valor esperado no retorno da função: 4 .
 */
+
+let arrayTeste = [2, 3, 6, 7, 10, 1];
+
+// for (number in arrayTeste)
+// {
+//   let maiorNumero = 0;
+
+//   let posicaoMaiorNumero = arrayTeste[number];
+//   console.log("Maior Número: " + maiorNumero);
+// }
+let maiorNumero = null;
+let posicaomaiorNumero = null;
+
+for (number in arrayTeste)
+{
+  if (arrayTeste[number] >= maiorNumero)
+  {
+    maiorNumero = arrayTeste[number];
+    posicaomaiorNumero = number;
+  }
+  else
+  {
+
+  }
+}
+console.log("Maior Número: " + maiorNumero);
+console.log("Posição Maior Número: " + posicaomaiorNumero);
