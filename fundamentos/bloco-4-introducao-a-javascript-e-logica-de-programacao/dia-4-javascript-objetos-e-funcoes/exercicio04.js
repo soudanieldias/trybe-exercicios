ecc4bb1864664a95119c9f7f684ee3ec0b9cfd3e
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-02 21:43:40 -0300

    Finaliza ecercicio04.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio04.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio04.js
@@ -12,3 +12,10 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
+
+info['recorrente'] = 'Sim';
+
+for (key in info)
+{
+    console.log(info[key]);
+}
