67bc0f79e0a45d356121aaf2e40328075fe15981
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-09 20:21:42 -0300

    Recupera paragrafpor class.

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/fixacao02.js
@@ -0,0 +1,9 @@
+// Adicione uma classe igual para os dois parágrafos.
+// Recupere os seus parágrafos via código JavaScript , usando a função getElementsByClassName ;
+// Altere algum estilo do primeiro deles.
+// Recupere o subtítulo e altere a cor dele usando a função getElementsByTagName.
+
+// conteudoParagrafos = getElementsByClassName("paragrafos");
+// console.log(conteudoParagrafos);
+
+console.log( document.getElementsByClassName("paragrafos") );
