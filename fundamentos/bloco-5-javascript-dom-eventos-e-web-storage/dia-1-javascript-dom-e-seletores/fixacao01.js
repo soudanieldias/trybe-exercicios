e2f575a1bbbb738af47695dd72d6204d401c2990
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-09 15:23:37 -0300

    Move conteúdo do <script> para arquivo separado.

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/fixacao01.js
@@ -0,0 +1,11 @@
+// Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito.
+      // Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
+      // Por fim, recupere o subtítulo e altere-o também.
+      const pageTitle = document.getElementById("page-title");
+      const paragraph = document.getElementById("paragraph");
+      const subTitle = document.getElementById("subtitle");
+      const secondParagraph = document.getElementById("second-paragraph");
+      paragraph.style.color = "red";
+      pageTitle.textContent = "Toy Story";
+      subtitle.textContent = "1995 ‧ Infantil/Comédia ‧ 1h 21m"
+      secondParagraph.textContent = "O aniversário do garoto Andy está chegando e seus brinquedos ficam nervosos, temendo que ele ganhe novos brinquedos que possam substituí-los. Liderados pelo caubói Woody, o brinquedo predileto de Andy, eles recebem Buzz Lightyear, o boneco de um patrulheiro espacial, que logo passa a receber mais atenção do garoto. Com ciúmes, Woody tenta ensiná-lo uma lição, mas Buzz cai pela janela. É o início da aventura do caubói, que precisa resgatar Buzz para limpar sua barra com os outros brinquedos.";
