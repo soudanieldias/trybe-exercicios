df83849d183147faf15ddf63478d59c769820ef6
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-10 18:57:35 -0300

    Acessa terceiroFilho do elementOndeEstou.

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/fixacao01.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/fixacao01.js
@@ -10,7 +10,7 @@
 
 // Acesse o elemento elementoOndeVoceEsta.
 let elementoOndeEstou = document.getElementById("elementoOndeVoceEsta");
-console.log(elementoOndeEstou); // Acessa elementoOndeVoceEsta
+// console.log(elementoOndeEstou); // Acessa elementoOndeVoceEsta
 // console.log(elementoOndeEstou.innerHTML); // Acessa conteúdo de elementoOndeVoceEsta como HTML
 
 // Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
@@ -32,7 +32,9 @@ let primeiroFilhoAtualElemento = elementoOndeEstou.firstChild;
 
 // Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
 let textoAtencao = elementoOndeEstou.parentNode.childNodes;
-console.log(textoAtencao[4]);
+// console.log(textoAtencao[4]);
 
-
+// Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+let terceiroFilho = elementoOndeEstou.childNodes;
+console.log(terceiroFilho[2]);
 
