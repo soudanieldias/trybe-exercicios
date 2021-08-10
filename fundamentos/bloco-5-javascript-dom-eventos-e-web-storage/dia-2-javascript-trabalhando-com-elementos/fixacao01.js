186554e66869862c079932f82b5afe18e44eb020
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-10 15:27:17 -0300

    Conclui 1º exercicio.

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/fixacao01.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/fixacao01.js
@@ -1,8 +1,14 @@
-// Acesse o elemento elementoOndeVoceEsta .
+// Acesse o elemento elementoOndeVoceEsta.
 // Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
 // Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
-// Acesse o primeiroFilho a partir de pai .
-// Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
-// Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
-// Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
-// Agora acesse o terceiroFilho a partir de pai .
+// Acesse o primeiroFilho a partir de pai.
+// Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+// Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+// Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+// Agora acesse o terceiroFilho a partir de pai.
+/********************************************************************/
+
+// Acesse o elemento elementoOndeVoceEsta.
+let elementoOndeEstou = document.getElementById("elementoOndeVoceEsta");
+console.log(elementoOndeEstou); // Acessa elementoOndeVoceEsta
+// console.log(elementoOndeEstou.innerHTML); // Acessa conteúdo de elementoOndeVoceEsta
