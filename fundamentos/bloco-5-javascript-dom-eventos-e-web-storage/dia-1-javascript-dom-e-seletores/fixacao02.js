cc4d462a93029692bbe60b05aa1ce9094b1194af
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-09 22:34:18 -0300

    Altera estilo do primeiro paragrafo.

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/fixacao02.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/fixacao02.js
@@ -3,7 +3,14 @@
 // Altere algum estilo do primeiro deles.
 // Recupere o subtítulo e altere a cor dele usando a função getElementsByTagName.
 
-// conteudoParagrafos = getElementsByClassName("paragrafos");
-// console.log(conteudoParagrafos);
+// console.log( document.getElementsByClassName("paragrafos") );
 
-console.log( document.getElementsByClassName("paragrafos") );
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+const conteudoParagrafos = document.getElementsByClassName("paragrafos");
+console.log(conteudoParagrafos);
+
+const paragrafo1 = document.getElementsByClassName("paragrafos")[0];
+
+paragrafo1.style.fontFamily = "Arial";
