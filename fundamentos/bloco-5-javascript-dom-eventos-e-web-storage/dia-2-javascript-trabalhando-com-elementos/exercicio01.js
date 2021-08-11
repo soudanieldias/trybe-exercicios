6ade30049d71b280a610375493eb6d6eb4f1fe93
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-11 17:35:16 -0300

    Adiciona indices restantes da Parte01.

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicio01.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicio01.js
@@ -29,3 +29,29 @@ const elementoMain = document.createElement('main');
 elementoMain.className = "main-content";
 elementoMain.innerText = "main-content///Filho da Body";
 document.body.appendChild(elementoMain);
+
+// Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
+
+
+
+// Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
+
+
+
+// Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+
+
+
+// Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
+
+
+
+// Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do section criado no passo 5;
+
+
+
+// Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um , dois , três , ... como valores da lista. Essa lista deve ser filha do section criado no passo 6;
+
+
+
+// Adicione 3 tags h3 , todas sendo filhas do main criado no passo 2.
