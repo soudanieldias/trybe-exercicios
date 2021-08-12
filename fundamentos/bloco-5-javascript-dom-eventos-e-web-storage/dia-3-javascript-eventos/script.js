5f003725108b58833d17b93314cbdeed9dc9914c
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-12 20:18:47 -0300

    Cria funçao que injeta dia no Calendario.

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -14,3 +14,25 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+  //  O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e Segunda-feira.
+
+  // Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
+  // Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
+  // Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>
+
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+  function criaDiasDoMes() {
+    let listaDosDias = document.getElementById('days');
+
+    for (let index = 0; index < dezDaysList.length; index +=1) {
+      let diaInput = dezDaysList[index];
+      // ESTRUTURA DEFAULT PARA INJETAR DIA DO ARRAY NA LISTA days
+      // let elementoDia = document.createElement('li');
+      // elementoDia.className = 'day';
+      // elementoDia.innerHTML = diaInput;
+      // listaDosDias.appendChild(elementoDia);
+    };
+  };
+  criaDiasDoMes();
