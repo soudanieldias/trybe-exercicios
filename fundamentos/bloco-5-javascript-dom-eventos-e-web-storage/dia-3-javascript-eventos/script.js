50815a82bfd925eb285ba3cb4588544c94dc151b
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-13 03:48:01 -0300

    Cria funcao adicionaTarefa.

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -169,3 +169,16 @@ itsFriday("Sexta-feira");
     // })
   // }
   // zoomOutDiaMes();
+
+  // 07. Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber
+  // como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento
+  // com a tag <span> contendo a tarefa.
+  // O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+
+  function adicionaTarefa(nomeTarefa) {
+    let listaTarefas = document.querySelector(".my-tasks");
+    let novaTarefa = document.createElement('span');
+    novaTarefa.innerText = nomeTarefa;
+    listaTarefas.appendChild(novaTarefa);
+  }
+  adicionaTarefa("Cozinhar");
