5478c02afa1dd1d9ff3da20e0dfe30c922bdbaf8
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-20 10:20:00 -0300

    Cria lista de estados Dinamicamente.

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicio01.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicio01.js
@@ -6,3 +6,13 @@ function handlesubmit(event) {
 let button = document.querySelector('.submit-button');
 console.log(button);
 button.addEventListener('click', handlesubmit);
+
+let estados = document.getElementById('state');
+let optionsEstado = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+console.log(estados);
+
+for ( let index = 0; index < optionsEstado.length; index += 1 ) {
+    let newState = document.createElement('option');
+    estados.appendChild(newState).innerText = optionsEstado[index];
+    estados.appendChild(newState).value = optionsEstado[index];
+}
