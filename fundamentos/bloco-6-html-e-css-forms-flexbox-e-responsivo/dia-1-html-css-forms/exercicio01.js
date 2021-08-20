b5042ab3e44fcb0e8bff995ba25406443da9dd3f
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-20 10:28:43 -0300

    Agrupa elementos de criaçao dos estados dentro de uma funçao.

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicio01.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicio01.js
@@ -7,12 +7,15 @@ let button = document.querySelector('.submit-button');
 console.log(button);
 button.addEventListener('click', handlesubmit);
 
-let estados = document.getElementById('state');
-let optionsEstado = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
-console.log(estados);
-
-for ( let index = 0; index < optionsEstado.length; index += 1 ) {
+function statesList () {
+  let estados = document.getElementById('state');
+  let optionsEstado = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+  let textEstado = ['Acre', 'Alagoas', 'Amazonas', 'Amapá', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiânia', 'Maranhão', 'Minas Gerais', 'Mato Grosso do Sul', 'Mato Grosso', 'Pará', 'Paraíba', 'Pernambuco', 'Piauí', 'Paraná', 'Rio de Janeiro', 'Rio Grando do Norte', 'Rondônia', 'Roraima', 'Rio Grande do SUl', 'Santa Catarina', 'Sergipe', 'São Paulo', 'Tocantins'];
+  // console.log(estados); // [DEBUG]
+  for ( let index = 0; index < optionsEstado.length; index += 1 ) {
     let newState = document.createElement('option');
-    estados.appendChild(newState).innerText = optionsEstado[index];
+    estados.appendChild(newState).innerText = textEstado[index];
     estados.appendChild(newState).value = optionsEstado[index];
+  }
 }
+statesList();
