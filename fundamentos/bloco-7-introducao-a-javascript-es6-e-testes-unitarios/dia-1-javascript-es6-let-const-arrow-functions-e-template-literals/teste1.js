be855c9233950422e470786f2942080473365b1f
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-27 14:49:22 -0300

    Executa teste parte I - [2].

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/teste1.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/teste1.js
@@ -2,6 +2,6 @@ function userInfo() {
     let userEmail = 'maria@email.com';
   
     // Toda expressão dentro do escopo da função userInfo tem acesso à variável userEmail
-    console.log(userEmail);
   }
+  console.log(userEmail);
   userInfo();
