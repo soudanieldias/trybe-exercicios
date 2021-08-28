53605499d9262483ebf4084cd847193ac608dd7a
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-28 19:13:02 -0300

    Adiciona arrow Function.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio01.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio01.js
@@ -3,7 +3,7 @@
  Modifique a estrutura da função para que ela seja uma arrow function.
  Modifique as concatenações para template literals.
 */
-function testingScope(escopo) {
+const testingScope = escopo => {
     if (escopo === true) {
       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
@@ -14,5 +14,4 @@ function testingScope(escopo) {
     }
     // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
   }
-
-  testingScope(true);
+  testingScope(false);
