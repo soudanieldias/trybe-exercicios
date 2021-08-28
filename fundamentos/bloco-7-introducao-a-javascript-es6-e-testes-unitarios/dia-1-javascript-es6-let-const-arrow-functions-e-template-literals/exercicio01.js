a31987e73d77e73a78a4c9a9f9a38c9b247099dc
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-28 19:04:03 -0300

    Conserta variavel para uso dentro do escopo.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio01.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio01.js
@@ -5,14 +5,14 @@
 */
 function testingScope(escopo) {
     if (escopo === true) {
-      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
       console.log(ifScope);
     } else {
-      var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
       console.log(elseScope);
     }
-    console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+    // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
   }
 
   testingScope(true);
