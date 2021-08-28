02026ea4ff38c9248bdd263176bbd07afbdb4073
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-28 20:20:20 -0300

    Executa teste de funçao .

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio02-2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio02-2.js
@@ -11,18 +11,33 @@ Crie uma função que receba uma frase e retorne qual a maior palavra.
 function recebeFrase(frase) {
   /* Função principal que recebe a Frase e se encarrega de chamar às demais funções para execução do processo. */
   console.log(`Frase recebida e Salva: '${frase}'`); // [DEBUG]
-  separaPalavras(frase);
+  const fraseSalva = frase;
+  console.log(`Frase Salva: ${fraseSalva}`);
+  separaPalavras(fraseSalva);
 }
 let frase = "O rato roeu a Roupa do Rei de Roma";
 recebeFrase(frase);
 
-function separaPalavras() {
+function separaPalavras(fraseSalva) {
   /* Separa Palavras e as coloca dentro de um array */
+  let arrayFrase = fraseSalva.split(' ');
+  console.log(`Array com Frase: ${arrayFrase}`);
+  verificaTamanho(arrayFrase);
 }
 
-function comparaPalavras() {
+function verificaTamanho(arrayFrase) {
   /*
     1. Recebe esse array de palavras, e através de um for verifica a maior palavra dentro do mesmo;
     2. Salva a palavra dentro da variável maiorPalavra;
   */
+  let maxLength = 0;
+    for (const palavra in arrayFrase) {
+      if (palavra.length > maxLength) {
+          maxLength = palavra.length;
+          result = palavra;
+          console.log(`Maior Palavra: ${result}\nTamanho da Palavra: ${palavra}`);
+      }
+      return result;
+  }
+  
 }
