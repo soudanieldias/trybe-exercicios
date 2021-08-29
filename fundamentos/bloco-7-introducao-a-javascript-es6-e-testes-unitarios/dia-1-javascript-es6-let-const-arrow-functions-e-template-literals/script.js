68dc9a5a08821cb3a31f3c7c0e29b3ca0e44b086
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-28 22:27:59 -0300

    Adiciona eventListener dentro da clickCounter e cria funçao anonima.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -2,7 +2,14 @@ let siteButton = document.getElementById('site-button');
 console.log(siteButton);
 
 function clickCounter() {
-    elementoP = document.querySelector('p');
+    const elementoP = document.querySelector('p');
+    let contador = 0;
     console.log(elementoP);
+    siteButton.addEventListener('click', function() {
+        contador+= 1;
+        // console.log(contador); // [DEBUG]
+        elementoP.innerText = contador;
+    });
+
 }
-siteButton.addEventListener('click', clickCounter);
+clickCounter();
