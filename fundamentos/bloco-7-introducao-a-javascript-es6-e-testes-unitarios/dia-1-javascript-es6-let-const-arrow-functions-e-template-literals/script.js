daf032fe61893749033b15c5bffdae122320fac2
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-28 22:24:58 -0300

    Testa funçao de click.

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,8 @@
+let siteButton = document.getElementById('site-button');
+console.log(siteButton);
+
+function clickCounter() {
+    elementoP = document.querySelector('p');
+    console.log(elementoP);
+}
+siteButton.addEventListener('click', clickCounter);
