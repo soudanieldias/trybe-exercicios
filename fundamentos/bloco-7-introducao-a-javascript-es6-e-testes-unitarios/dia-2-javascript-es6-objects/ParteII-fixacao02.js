14f636ae13745900391a55c51b8e4c1f5046a668
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-30 15:50:59 -0300

    Testa exercicio fixaçao parte II.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/ParteII-fixacao02.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/ParteII-fixacao02.js
@@ -1,21 +1,44 @@
-var arr = ['a', 'b', 'c'];
-console.log(`Chave: ${Object.keys(arr)} \nValor: ${Object.values(arr)}`);
+const student1 = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkills: 'Ótimo',
+  };
+  
+  const student2 = {
+    Html: 'Bom',
+    Css: 'Ótimo',
+    JavaScript: 'Ruim',
+    SoftSkills: 'Ótimo',
+    Git: 'Bom', // chave adicionada
+  };
 
-// array com objeto
-var obj = { 0: 'a', 1: 'b', 2: 'c' };
-console.log(`Chave: ${Object.keys(obj)} \nValor: ${Object.values(obj)}`); // console: ['0', '1', '2']
+  //console.log(`${Object.keys(student1)} ${Object.values(student1)}`); //Imprime Valores Soltos
 
-// array como objeto com ordenação aleatória por chave
-var an_obj = { 100: 'a', 2: 'b', 7: 'c' };
-console.log(Object.keys(an_obj)); // console: ['2', '7', '100']
+  const listSkills = (student) => {
+    const arrayOfSkills = Object.keys(student);
+    for(index in arrayOfSkills){
+    //   console.log("1º LOG:");
+      console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+    //   console.log("**********");
+    }
+  };
+  console.log('Estudante 1:');
+  listSkills(student1);
+  console.log("**********");
 
-// getFoo é uma propriedade que não é enumerável
-var my_obj = Object.create({}, { getFoo: { value: function() { return this.foo; } } });
-my_obj.foo = 1;
-console.log(Object.keys(my_obj)); // console: ['foo']
+  console.log('Estudante 2:');
+  listSkills(student2);
+  console.log("**********");
 
-Object.keys("foo");
-// TypeError: "foo" is not an object (ES5 code)
-
-Object.keys("foo");
-// ["0", "1", "2"]                   (ES2015 code)
+  console.log("LOG CHAVE <=> VALUE ESTUDANTE 01:");
+  for (values in student1) {
+    
+    console.log(`${values}: ${student1[values]}`);
+  }
+  
+  console.log("LOG CHAVE <=> VALUE ESTUDANTE 02:");
+  for (values in student2) {
+    
+    console.log(`${values}: ${student2[values]}`);
+  }
