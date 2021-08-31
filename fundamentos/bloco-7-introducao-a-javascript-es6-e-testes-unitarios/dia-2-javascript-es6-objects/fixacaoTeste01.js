e4128d1be20b8bf4361285814ec93daa73bddad4
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-31 00:31:11 -0300

    Adiciona teste de fixação.

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/fixacaoTeste01.js
@@ -0,0 +1,10 @@
+const starships = {
+    option1: "Tardis",
+    option2: "Millennium Falcon",
+    option3: "Enterprise",
+    option4: "Heart of Gold"
+  }
+   let allValues = Object.values(starships);
+   let allEntries = Object.entries(starships);
+   console.log(allValues);
+   console.log(allEntries);
