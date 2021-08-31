350c70703a2fb1d239be4722a568b937d781f82d
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-31 14:30:10 -0300

    Finaliza exercício 07.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/exercicio02.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/exercicio02.js
@@ -33,23 +33,23 @@ const lesson1 = {
 
 // 1- Crie uma função para adicionar o turno da manhã na lesson2.
 let setParams = (objName, objKey, keyValue) => objName[objKey] = keyValue;
-console.log(setParams(lesson2, 'turno', 'manhã'));
+// console.log(setParams(lesson2, 'turno', 'manhã'));
 
 // 2- Crie uma função para listar as keys de um objeto.
 let listKeys = objName => `Keys do Object: ${Object.keys(objName)}`;
-console.log(listKeys(lesson3));
+// console.log(listKeys(lesson3));
 
 // 3- Crie uma função para mostrar o tamanho de um objeto.
 let objLenght = objName =>  `Tamanho: ${Object.keys(objName).length}`;
-console.log(objLenght(lesson1));
+// console.log(objLenght(lesson1));
 
 // 4- Crie uma função para listar os valores de um objeto.
 let listValues = objName => `Values do Object: ${Object.values(objName)}`;
-console.log(listValues(lesson3));
+// console.log(listValues(lesson3));
 
 // 5- Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign.
 let allLessons = (obj1, obj2, obj3) => Object.assign({}, {obj1}, {obj2}, {obj3});
-console.log(allLessons(lesson1, lesson2, lesson3));
+// console.log(allLessons(lesson1, lesson2, lesson3));
 
 // 6- Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas. 
 let totalAlunos = allLessons => {
@@ -61,20 +61,27 @@ let totalAlunos = allLessons => {
     console.log(`Total de Alunos: ${totalDeAlunos}`);
 };
 let functionAllLessons = allLessons(lesson1, lesson2, lesson3);
-totalAlunos(functionAllLessons);
+// totalAlunos(functionAllLessons);
 
 // 7- Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
 let getValueByNumber = (objName, keyPosition) => console.log(Object.keys(objName)[keyPosition]);
-getValueByNumber(lesson1, 3);
+// getValueByNumber(lesson1, 3);
 
 /*
 8- Crie uma função que verifique se o par (chave / valor) existe na função.
  -> Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave.
 */
 let checkPair = (obj, key, value) => {
-  for (key in obj) {
-    // console.log( Object.entries(key) );
-    console.log(`${key} ${value}`)
+
+  if (key in obj) {
+    // console.log(obj[key]);
+    if (obj[key] === value) {
+      console.log(`True`);
+    } else {
+        console.log(false);
+    }
+  } else {
+    console.log(false);
   }
 }
-checkPair(lesson1, 'numeroEstudantes', 30);
+checkPair(lesson1, 'numeroEstudantes', 20);
