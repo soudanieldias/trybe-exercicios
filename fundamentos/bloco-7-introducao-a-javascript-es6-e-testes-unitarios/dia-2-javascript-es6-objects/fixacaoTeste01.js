9746a422acaa9933dd18f9b0108b35c6f1b3d8ab
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-31 00:37:04 -0300

    Realiza testes do codigo.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/fixacaoTeste01.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/fixacaoTeste01.js
@@ -4,7 +4,16 @@ const starships = {
     option3: "Enterprise",
     option4: "Heart of Gold"
   }
-   let allValues = Object.values(starships);
-   let allEntries = Object.entries(starships);
-   console.log(allValues);
-   console.log(allEntries);
+let allValues = Object.values(starships);
+let allEntries = Object.entries(starships);
+console.log(allValues);
+console.log(allEntries);
+
+const top3Albums = {
+    1 : 'Nightfall in Middle-Earth',
+    2 : 'Imaginations from the Other Side',
+    3 : 'Somewhere Far Beyond',
+};
+
+let top3Entries = Object.entries(top3Albums);
+console.log(top3Entries);
