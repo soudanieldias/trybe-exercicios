b13c0e1dad2c9dd64149ce03487ff54209f9ac8f
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-31 22:28:40 -0300

    Adiciona resoluçao bônus 2.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/exercicio03-bonus.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/exercicio03-bonus.js
@@ -40,3 +40,25 @@ const countMathStudents = obj => {
 }
 let auxMath = allLessons(lesson1, lesson2, lesson3);
 countMathStudents(auxMath);
+
+// 2- Crie uma função que deverá retornar um objeto que representa o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes.
+const getInfo = (obj, name) => {
+  const allLessons = [];
+  let allStudent = 0;
+  const array = Object.values(obj);
+  for (index in array) {
+    if (array[index].professor === name) {
+      allLessons.push(array[index].materia)
+      allStudent += array[index].numeroEstudantes;
+    }
+  }
+  return { lessons: allLessons, estudantes: allStudent };
+}
+
+const createReport = (allLessons, name) => {
+  const report = {};
+  report.professor = name;
+  Object.assign(report, getInfo(allLessons, name));
+  return report;
+}
+console.log(createReport(allLessons, 'Maria Clara'));
