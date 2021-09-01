834ca4ef7af78553182245f56d7944614bb74de8
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-01 02:06:21 -0300

    Implementa 2º parâmetro como string e testa, aguardando  erro.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/exercicio01.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/exercicio01.js
@@ -21,3 +21,7 @@ assert.strictEqual(sum(4, 5), 9, 'ERRO: 4 + 5 = 9');
 
 // 1.2 - Teste se o retorno de sum(0, 0) é 0
 assert.strictEqual(sum(0, 0), 0, 'ERRO: 0 + 0 = 0');
+
+// 1.3 - Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+// 1.4 - Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+assert.strictEqual(sum(4, "5"), 9, "ERRO: 4 + 5 = 9");
