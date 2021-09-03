e9b580f55378b0b746e9d3534ecc92ce6986b506
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-03 04:25:39 -0300

    Modifica forma de armazenamento de results, de = para push.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/exercicio13.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/exercicio13.js
@@ -2,13 +2,19 @@
 // para verificar se a mesma está retornando como se vê na variável result e,
 // caso não esteja, altere o código para que ele passe nos testes. 
 function secondThirdSmallest(array) {
-  let results = []
+  let results = [];
   array.sort(function (x, y) {
-      return x + y;
+      return x - y;
   });
-  results = [array[1], array[2]];
+  results.push( array[1], array[2]);
   return results;
 };
 
 const parameter = [4, 10, 32, 9, 21, 90, 5, 11, 8, 6];
 const result = [5, 6];
+
+const assert = require('assert');
+
+assert.strictEqual(typeof secondThirdSmallest, 'function');
+const output = secondThirdSmallest(parameter);
+assert.deepStrictEqual(output, result);
