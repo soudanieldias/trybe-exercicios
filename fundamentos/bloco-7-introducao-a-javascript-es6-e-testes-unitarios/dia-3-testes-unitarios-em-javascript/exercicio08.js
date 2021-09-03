6b220ea2bbe9b0ab8bebc28ff7eb2100d6e590cb
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-03 02:24:20 -0300

    Adiciona funçao sumAllNumbers.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/exercicio08.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/exercicio08.js
@@ -1,7 +1,13 @@
 // Escreva a função sumAllNumbers para passar nos testes já implementados. 
 const assert = require('assert');
 // escreva a função sumAllNumbers aqui
-
+const sumAllNumbers = (numbers) => {
+    let sum = 0;
+    for (let index = 0; index < numbers.length; index += 1) {
+        sum += numbers[index];
+    }
+    return sum;
+}
 const numbers = [9, 23, 10, 3, 8];
 const expected = 53;
 const output = sumAllNumbers(numbers);
