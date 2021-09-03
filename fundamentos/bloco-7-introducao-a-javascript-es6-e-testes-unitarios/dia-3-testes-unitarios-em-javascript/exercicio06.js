0eb691e60147779e96b6986de45df6cf6f90be85
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-03 01:39:03 -0300

    Adiciona funçao addOne que modifica diretamente o valor do input.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/exercicio06.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/exercicio06.js
@@ -3,7 +3,11 @@
 const assert = require('assert');
 // escreva a função addOne aqui
 function addOne (input) {
-  // 
+  // Função recebe input de array, adiciona 1 a cada elemento numérico
+  for (let index = 0; index < input.length; index += 1) {
+      input[index] += 1;
+  }
+  return input;
 }
 
 const myArray = [31, 57, 12, 5];
