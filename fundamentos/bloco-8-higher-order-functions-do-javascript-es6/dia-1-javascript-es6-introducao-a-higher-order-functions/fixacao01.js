49232ca167c7a150af07e1300dc2c32249b020da
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-05 02:06:29 -0300

    Adiciona indice das funçoes.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/fixacao01.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/fixacao01.js
@@ -5,12 +5,14 @@
 // 4 - Agora desenvolva uma HOF chamada doingThings e configure esta função para que imprima no console o resultado da execução das funções que você criou nos exemplos anteriores.
 
 // 1 - Crie uma função que retorne a string 'Acordando!!';
-
 const wakeUp = () => console.log('Acordando!!');
 wakeUp();
 
+// 2 - Crie outra função que retorne a string 'Bora tomar café!!';
 const timeToCoffee = () => console.log('Bora tomar café!!');
 timeToCoffee();
 
+// 3 - Crie mais uma função que retorne a string 'Partiu dormir!!';
 const timeToSleep = () => console.log('Partiu dormir!!');
 timeToSleep();
+
