8f58786abd4b1117a22fd7d91a505f95f6198a8a
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-05 02:13:30 -0300

    Reformulada HoF.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/fixacao01.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/fixacao01.js
@@ -18,10 +18,10 @@ const timeToSleep = () => 'Partiu dormir!!'
 
 // 4 - Agora desenvolva uma HOF chamada doingThings e configure esta função para que
 // imprima no console o resultado da execução das funções que você criou nos exemplos anteriores.
-const doingThings = (func1, func2, func3) => {
-    console.log(func1);
-    console.log(func2);
-    console.log(func3);
+const doingThings = (func) => {
+    console.log(func);
 }
-doingThings(wakeUp(), timeToCoffee(), timeToSleep());
+doingThings(wakeUp());
+doingThings(timeToCoffee());
+doingThings(timeToSleep());
 
