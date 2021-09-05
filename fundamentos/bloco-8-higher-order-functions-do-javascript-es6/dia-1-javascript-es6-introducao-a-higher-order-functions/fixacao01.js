9d04585ada3862c87aed793eb1a79926e8e85dc5
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-05 02:05:55 -0300

    Cria 3 primeiras funçoes.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/fixacao01.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/fixacao01.js
@@ -5,3 +5,12 @@
 // 4 - Agora desenvolva uma HOF chamada doingThings e configure esta função para que imprima no console o resultado da execução das funções que você criou nos exemplos anteriores.
 
 // 1 - Crie uma função que retorne a string 'Acordando!!';
+
+const wakeUp = () => console.log('Acordando!!');
+wakeUp();
+
+const timeToCoffee = () => console.log('Bora tomar café!!');
+timeToCoffee();
+
+const timeToSleep = () => console.log('Partiu dormir!!');
+timeToSleep();
