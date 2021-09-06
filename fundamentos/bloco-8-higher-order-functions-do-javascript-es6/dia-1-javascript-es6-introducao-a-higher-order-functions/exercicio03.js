34118049e0afdd85b4995efcbc6c7059bdd4b3fa
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-06 03:02:53 -0300

    Estrutura funçoes do exercicio03.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio03.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio03.js
@@ -8,3 +8,11 @@
 
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const countCorrect = () => {
+    
+}
+const newHof = (rigAns, studAns, callback) => {
+
+}
+// (RIGHT_ANSWERS, STUDENT_ANSWERS, callback)
