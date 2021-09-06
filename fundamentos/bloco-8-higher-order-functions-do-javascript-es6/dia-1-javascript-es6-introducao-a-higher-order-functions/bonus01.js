04448904f02e874faf93b2959f6c74063b3076cc
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-06 14:00:57 -0300

    Adiciona indice do exercicio bonus01.

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonus01.js
@@ -0,0 +1,26 @@
+/*
+Crie uma função que retorna o dano do dragão.
+  *** O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo).
+*/
+
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
