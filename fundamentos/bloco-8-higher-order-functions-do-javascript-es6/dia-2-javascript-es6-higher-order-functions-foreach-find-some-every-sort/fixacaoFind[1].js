181caf9a14ea2ea9f84d6e71ab12e4dfdd275741
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-07 23:14:45 -0300

    Refatora funçao para utilizar find.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixacaoFind[1].js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixacaoFind[1].js
@@ -1,8 +1,5 @@
 const numbers = [19, 21, 30, 3, 45, 22, 15];
 
-const findDivisibleBy3And5 = () => {
-  // Adiciona seu código aqui
-  
-}
+const findDivisibleBy3And5 = numbers.find((number) => number % 3 === 0 && number % 5 === 0 );
 
-console.log(findDivisibleBy3And5())
+console.log(findDivisibleBy3And5)
