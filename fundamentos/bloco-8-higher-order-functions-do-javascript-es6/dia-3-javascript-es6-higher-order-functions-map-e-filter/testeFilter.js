514e6cac2fab8df5659e0d13a9bd29d5bd7a7531
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-09 11:24:01 -0300

    Adiciona testeFilter.js, com teste da funçao filter para entendimento.

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/testeFilter.js
@@ -0,0 +1,28 @@
+// console.log("TESTE 1");
+// console.log("TESTE 2");
+// console.log("TESTE 3");
+// console.log("TESTE 4");
+// console.log("TESTE 5");
+// console.log("TESTE 6");
+// console.log("TESTE 7");
+// console.log("TESTE 8");
+// console.log("TESTE 9");
+// console.log("TESTE 10");
+// console.log("TESTE 11");
+// console.log("TESTE 12");
+// console.log("TESTE 13");
+// console.log("TESTE 14");
+// console.log("TESTE 15");
+// console.log("TESTE 16");
+// console.log("TESTE 17");
+// console.log("TESTE 18");
+// console.log("TESTE 19");
+// console.log("TESTE 20");
+
+const arrayDeElementos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const resultadoFilter = arrayDeElementos.filter( (elemento) => elemento > 5);
+
+console.log(resultadoFilter);
+
+resultadoFilter.forEach( (element) => console.log(element) );
