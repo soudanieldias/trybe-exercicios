dbb38ee097ee18a16f0e8bf4ba1fb38e2c0448ca
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-09 11:39:22 -0300

    Remove elementos desnecessarios.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/testeFilter[01].js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/testeFilter[01].js
@@ -1,24 +1,3 @@
-// console.log("TESTE 1");
-// console.log("TESTE 2");
-// console.log("TESTE 3");
-// console.log("TESTE 4");
-// console.log("TESTE 5");
-// console.log("TESTE 6");
-// console.log("TESTE 7");
-// console.log("TESTE 8");
-// console.log("TESTE 9");
-// console.log("TESTE 10");
-// console.log("TESTE 11");
-// console.log("TESTE 12");
-// console.log("TESTE 13");
-// console.log("TESTE 14");
-// console.log("TESTE 15");
-// console.log("TESTE 16");
-// console.log("TESTE 17");
-// console.log("TESTE 18");
-// console.log("TESTE 19");
-// console.log("TESTE 20");
-
 // Cria-se um Array
 const arrayDeElementos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
 
