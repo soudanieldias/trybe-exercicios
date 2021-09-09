5fae60dbff9735127db2c01b1628ba3165bc37ab
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-09 11:35:32 -0300

    Altera nome do arquivo // Adiciona comentarios as funçoes.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/testeFilter[01].js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/testeFilter[01].js
@@ -19,10 +19,15 @@
 // console.log("TESTE 19");
 // console.log("TESTE 20");
 
-const arrayDeElementos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// Cria-se um Array
+const arrayDeElementos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
 
-const resultadoFilter = arrayDeElementos.filter( (elemento) => elemento > 5);
+// Armazena a função filter, que retorna true cada vez que a função for satisfeita, em uma variável
+// Juntamente desse true, é armazenado o elemento da posição em formato de array
+const resultadoFilter = arrayDeElementos.filter( (elemento) => elemento > 5); 
 
+// É executado o console.log do array resultadoFilter
 console.log(resultadoFilter);
 
+// É testado o forEach para 
 resultadoFilter.forEach( (element) => console.log(element) );
