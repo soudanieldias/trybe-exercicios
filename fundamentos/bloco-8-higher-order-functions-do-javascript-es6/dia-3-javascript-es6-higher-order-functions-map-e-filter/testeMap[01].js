db4d818f3a4361414eaf985589ef63ee6316d956
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-09 11:47:27 -0300

    Cria arquivo, e adiciona arrayEmail para trabalhar com map.

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/testeMap[01].js
@@ -0,0 +1,9 @@
+const arrayEmail = [
+    'ada.lovelace@example.com',
+    'alan.turing@example.com',
+    'margaret.hamilton@example.com',
+    'marie.curie@example.com',
+    'albert.einstein@example.com',
+    'pierre.curie@example.com',
+    'stephen.hawking@example.com'
+]
