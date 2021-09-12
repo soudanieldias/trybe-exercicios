56379080bbe2c5aba58fe5137e88a7efd96b9d84
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-12 16:38:31 -0300

    Finaliza exercicio01.js

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercícios/exercicio01.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercícios/exercicio01.js
@@ -7,11 +7,16 @@ const assert = require('assert');
 
 const rectangleArea = (width, height) => width * height;
 
+// const rectangleArea = (width, height) => {
+//     // console.log(width * height) // [DEBUG] Verifica retorno da função
+//     return(width * height)
+// };
+
 const rectangle1 = [1, 2];
 const rectangle2 = [3, 5];
 const rectangle3 = [6, 9];
 const rectangles = [rectangle1, rectangle2, rectangle3];
 
 rectangles.forEach((rectangle) => {
-  assert.strictEqual(rectangleArea(), rectangle[0] * rectangle[1]); // altere a chamada da funcao rectangleArea
+  assert.strictEqual(rectangleArea(...rectangle), rectangle[0] * rectangle[1]); // altere a chamada da funcao rectangleArea
 });
