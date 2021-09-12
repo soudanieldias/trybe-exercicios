8f4e103621bf30f1d567ccdbffd83846f333146e
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-12 11:58:43 -0300

    Adiciona 2o teste ao testeRest[02].

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/testeRest[02].js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/testeRest[02].js
@@ -1,5 +1,5 @@
 const sum = (...args) => args.reduce((accumulator, current) => accumulator + current, 0);
 console.log(sum(4, 7, 8, 9, 60)); // 88
 
-const arrayASomar = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
-console.log(sum(...arrayASomar));
+const arrayASomar = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]; // Define o array a somar
+console.log(sum(...arrayASomar)); // Joga todos os elementos a serem somados, como parâmetro da função sum
