1de38503704ee74f95dfe8f0c26c2e139430957d
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-12 12:01:21 -0300

    Testa Destructuring.

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/testeDestructuring[01].js
@@ -0,0 +1,8 @@
+const product = {
+    name: 'Smart TV Crystal UHD',
+    price: '1899.05',
+    seller: 'Casas de Minas',
+  };
+
+const { name } = product;
+console.log(name); // Smart TV Crystal UHD
