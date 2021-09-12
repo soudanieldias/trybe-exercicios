a4455b99416847b798493bae0e8d676efdef4a07
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-12 14:47:12 -0300

    Finaliza fixacao01 do arrayDestructuring.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixação/fixacaoArrayDestructuring[01].js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixação/fixacaoArrayDestructuring[01].js
@@ -1,5 +1,8 @@
 const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
 
-saudacoes[1](saudacoes[0]); // Olá
+// saudacoes[1](saudacoes[0]); // Olá
 
 // Produza o mesmo resultado acima, porém utilizando array destructuring
+const [textoSaudacao, funcaoSaudacao] = saudacoes;
+
+console.log(funcaoSaudacao(textoSaudacao) );
