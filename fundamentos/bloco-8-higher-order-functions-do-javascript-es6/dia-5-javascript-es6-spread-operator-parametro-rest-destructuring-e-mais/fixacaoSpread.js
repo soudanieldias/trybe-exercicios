385456d4f6a92e668a046e62529f1b0586f92794
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-12 11:34:06 -0300

    Adiciona itens e cria funçao.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixacaoSpread.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixacaoSpread.js
@@ -1,11 +1,12 @@
 // Faça uma lista com as suas frutas favoritas
-const specialFruit = ['', '', ''];
+const specialFruit = ['Manga', 'Banana', 'Maça'];
 
 // Faça uma lista de complementos que você gostaria de adicionar
-const additionalItens = ['', '', ''];
+const additionalItens = ['Leite condensado', 'Creme de leite', 'Canela'];
 
 const fruitSalad = (fruit, additional) => {
   // Esreva sua função aqui
+  return [...fruit, ...additional];
 };
 
 console.log(fruitSalad(specialFruit, additionalItens));
