bc25e7071e8a0f33c05291a28fec767fb18e36dc
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-12 14:59:11 -0300

    Adiciona resoluçao fixacao03 arrayDestructuring.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixação/fixacaoArrayDestructuring[03].js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixação/fixacaoArrayDestructuring[03].js
@@ -5,6 +5,9 @@
 
 let numerosPares = [1, 3, 5, 6, 8, 10, 12];
 
-console.log(numerosPares); // [6, 8, 10, 12];
+console.log(numerosPares); // array Inalterado
 
+numerosPares = [numerosPares[3], numerosPares[4], numerosPares[5], numerosPares[6]];
+
+console.log(numerosPares); // [6, 8, 10, 12];
 // Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
