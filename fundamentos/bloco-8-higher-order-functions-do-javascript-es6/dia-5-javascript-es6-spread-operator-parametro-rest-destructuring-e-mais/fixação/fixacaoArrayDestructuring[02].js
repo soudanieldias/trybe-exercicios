b223bade90574746c29fba7000903c37ddc3f336
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-12 14:54:31 -0300

    Finaliza fixacao02 do arrayDestructuring.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixação/fixacaoArrayDestructuring[02].js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixação/fixacaoArrayDestructuring[02].js
@@ -7,6 +7,14 @@ let comida = 'gato';
 let animal = 'água';
 let bebida = 'arroz';
 
-console.log(comida, animal, bebida); // arroz gato água
+console.log(`Variáveis inalteradas:\nComida: ${comida}\nAnimal: ${animal}\nBebida: ${bebida}`); // arroz gato água
+
+let aux = comida;
+comida = bebida;
+bebida = animal;
+animal = aux;
+
+console.log(`Variáveis Alteradas:\nComida: ${comida}\nAnimal: ${animal}\nBebida: ${bebida}`); // arroz gato água
+
 
 // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
