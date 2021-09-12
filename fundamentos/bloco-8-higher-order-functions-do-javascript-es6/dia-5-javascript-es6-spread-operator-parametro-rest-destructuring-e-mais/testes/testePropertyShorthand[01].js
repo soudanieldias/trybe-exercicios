5f1ae0f790c2cc506c8d8cc9b611654884cc7eac
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-12 15:37:06 -0300

    Adiciona teste da Property Shorthand.

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/testes/testePropertyShorthand[01].js
@@ -0,0 +1,9 @@
+const newUser = (id, name, email) => {
+  return {
+    id: id,
+    name: name,
+    email: email,
+  };
+};
+
+console.log(newUser(54, 'isabella', 'isabella@email.com')); // { id: 54, name: 'isabella', email: 'isabella@email.com' }
