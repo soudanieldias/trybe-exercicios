f76948c48242f8f87ccd391019b82b749b5be0fb
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-12 00:47:20 -0300

    Testa funçao que busca maior livro.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicio04.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicio04.js
@@ -64,3 +64,34 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+// Encontre o livro com o maior nome.
+
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+};
+
+// LÓGICA DE RESOLUÇÃO:
+// ** Varrer o array dos livros buscando pelos nomes
+// ** Comparar os elementos de maior valor
+// ** Retornar o Obj completo do elemento com aquele nome, contendo id, nome, genero, dados do autor, e ano de lançamento
+
+function longestNamedBook() {
+  // escreva seu código aqui
+  const findLongestName = (maiorLivro, livroAtual) => { // Função findLongestName, que recebe accumulator, actualValue
+    if (livroAtual.name.length > maiorLivro.name.length) { // Se o tamanho do nome do livro sendo comparado, for maior do que o anterior, o livro atual passa a ser o maior livro
+      return livroAtual; // Atribui o obj livro atual ao actualValue
+    }
+    return maiorLivro; // Se não, mantém o obj anterior ao comparado imutável, como sendo o maior livro
+  };
+  return reduceBook = books.reduce(findLongestName);
+}
+// console.log( longestNamedBook() ); // [DEBUG] Verifica livro retornado pela função longestNamedBook
+assert.deepStrictEqual(longestNamedBook(), expectedResult);
