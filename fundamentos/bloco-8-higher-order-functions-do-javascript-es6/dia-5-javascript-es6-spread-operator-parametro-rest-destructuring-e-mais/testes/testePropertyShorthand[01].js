8093bb3b13d950764a8e43cbcd400179b5ad20d3
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-12 15:39:07 -0300

    Adiciona e testa PropertyShorthand modificado.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/testes/testePropertyShorthand[01].js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/testes/testePropertyShorthand[01].js
@@ -7,3 +7,13 @@ const newUser = (id, name, email) => {
 };
 
 console.log(newUser(54, 'isabella', 'isabella@email.com')); // { id: 54, name: 'isabella', email: 'isabella@email.com' }
+
+const novoUsuario = (id, name, email) => {
+  return {
+    id,
+    name,
+    email,
+  };
+};
+
+console.log(novoUsuario(30, 'Daniel', 'daniel@email.com')); // { id: 30, name: 'Daniel', email: 'daniel@email.com' }
