214ce7a3ef534a1e57b2692bd2ac60981a43e35f
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-21 22:25:27 -0300

    Separa testes em 2 describes.

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/my_new_project/tests/testDescribe.spec.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/my_new_project/tests/testDescribe.spec.js
@@ -1,12 +1,14 @@
 const workDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 const weekDays = ['Sunday', ...workDays, 'Saturday'];
 
-describe('Testa duplo dentro do Describe', () => {
+describe('Testa Sunday week day', () => {
     test('Sunday is a week day', () => {
         expect(weekDays).toContain('Sunday');
       });
-      
-      test('Sunday is not a workday', () => {
+});
+
+describe('Testa Sunday not a workday', () => {
+    test('Sunday is not a workday', () => {
         expect(workDays).not.toContain('Sunday');
       });
-})
+});
