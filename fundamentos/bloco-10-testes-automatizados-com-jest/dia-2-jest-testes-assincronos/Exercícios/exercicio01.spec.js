901b873514cdb85eca501fbd09cc6b9b70cb98eb
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-23 14:37:24 -0300

    Finaliza 1o teste exercicio01.spec.js

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/Exercícios/exercicio01.spec.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/Exercícios/exercicio01.spec.js
@@ -9,4 +9,20 @@ const uppercase = (str, callback) => {
       callback(str.toUpperCase());
     }, 500);
   };
-  
+
+  describe('Testa Função upperCase', done => {
+    test('Verifica se passado uma palavra para a função, todas as letras retornam MAIÚSCULAS', () => {
+
+      function callback(data) {
+        
+        try {
+          expect(data).toBe('STRING');
+          done();
+
+        } catch (error) {
+          done(error);
+        }
+      }
+      uppercase('string', callback);
+    });
+  });
