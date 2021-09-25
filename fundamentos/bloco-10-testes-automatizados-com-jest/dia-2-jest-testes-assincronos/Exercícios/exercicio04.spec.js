11fe197876e4f7c4f5d15ce2818e47782ce780aa
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-25 13:45:06 -0300

    Realiza Teste.

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/Exercícios/exercicio04.spec.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/Exercícios/exercicio04.spec.js
@@ -15,4 +15,18 @@
        return data.map((repo) => repo.name);
      });
  };
+
+//  console.log(getRepos('https://api.github.com/orgs/tryber/repos'));
+
+describe('Realiza Suite de testes no código', () => {
+
+  test('Teste01', () => {
+
+    const trybeURL = 'https://api.github.com/orgs/tryber/repos';
+
+    expect.assertions(1);
+
+    return getRepos(trybeURL).then(data => expect(data).toBe(["sd-01-week3-html-css-project", "sd-01-week4-5-project-meme-generator", "sd-01-week4-5-project-mystery-letter", "sd-01-week4-5-project-pixels-art", "sd-01-week4-5-project-talk-pirate", "sd-01-week4-5-project-color-guess", "sd-01-week4-5-project-todo-list", "sd-01-week6-project-facebook-signup", "sd-01-week7-project-daily-prophet", "sd-02-week2-project-shell-script", "sd-01-week8-project-zoo-functions", "sd-01-week9-project-shopping-cart", "sd-01-week10-movie-card-library", "sd-01-week11-movie-card-library-stateful", "sd-02-week5-project-meme-generator", "sd-02-week5-project-pixels-art", "sd-02-week5-project-color-guess", "sd-02-week5-project-todo-list", "sd-02-week5-project-mystery-letter", "sd-02-week6-project-facebook-signup", "sd-02-week7-project-daily-prophet", "sd-02-week8-project-zoo-functions", "sd-02-week9-project-shopping-cart", "sd-02-week11-movie-card-library", "sd-02-week12-movie-card-library-stateful", "sd-01-week12-movie-card-library-crud", "sd-01-week13-project-frontend-online-store", "sd-01-week13-project-frontend-online-store-group-2", "linter-template", "sd-01-week14-js-unit-tests"]));
+  });
+});
  
