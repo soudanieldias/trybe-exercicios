7dd04fb1987e465bade53b25f3d0c42f972bd9e3
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-26 10:23:57 -0300

    Adiciona resoluçao 06-01.spec.js

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/Exercícios/exercicio06-01.spec.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/Exercícios/exercicio06-01.spec.js
@@ -1,28 +1,40 @@
-/**
- * Adicione uma funcionalidade para buscar pelo nome do animal
- *  (crie uma função que deverá passar no teste abaixo).
- * Dica: use o código do exemplo dado para criar uma nova função, analise os testes antes de iniciar.
- */
-
 const Animals = [
-    { name: 'Dorminhoco', age: 1, type: 'Dog' },
-    { name: 'Soneca', age: 2, type: 'Dog' },
-    { name: 'Preguiça', age: 5, type: 'Cat' },
-  ];
-  
-  const findAnimalsByType = (type) => (
-    new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const arrayAnimals = Animals.filter((animal) => animal.type === type);
-        if (arrayAnimals.length !== 0) {
-          return resolve(arrayAnimals);
-        };
-  
-        return reject({ error: 'Não possui esse tipo de animal.' });
-      }, 100);
-    })
-  );
-  
-  const getListAnimals = (type) => (
-    findAnimalsByType(type).then(list => list)
-  );
+  { name: 'Dorminhoco', age: 1, type: 'Dog' },
+  { name: 'Soneca', age: 2, type: 'Dog' },
+  { name: 'Preguiça', age: 5, type: 'Cat' },
+];
+
+// [...]
+
+const findAnimalByName = (name) => (
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const result = Animals.find((animal) => animal.name === name);
+
+      if (result) {
+        return resolve(result);
+      }
+
+      return reject(new Error('Nenhum animal com esse nome!'));
+    }, 100);
+  })
+);
+
+describe('Testando promise - findAnimalByName', () => {
+  describe('Quando existe o animal com o nome procurado', () => {
+    test('Retorne o objeto do animal', () => (
+      findAnimalByName('Dorminhoco').then((animal) => {
+        expect(animal).toEqual({ name: 'Dorminhoco', age: 1, type: 'Dog' });
+      })
+    ));
+  });
+
+  describe('Quando não existe o animal com o nome procurado', () => {
+    test('Retorna um erro', () => {
+      expect.assertions(1);
+      return findAnimalByName('Bob').catch((error) => {
+        expect(error.message).toEqual('Nenhum animal com esse nome!');
+      });
+    });
+  });
+});
