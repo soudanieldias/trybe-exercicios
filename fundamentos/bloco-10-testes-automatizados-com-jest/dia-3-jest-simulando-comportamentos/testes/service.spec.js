9384f2745e948dce00810453b2107f5fef23a90d
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-26 20:23:58 -0300

    Adiciona Mock a service.js

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-3-jest-simulando-comportamentos/testes/service.spec.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-3-jest-simulando-comportamentos/testes/service.spec.js
@@ -1,12 +1,21 @@
 const service = require('./service');
 
 test("#randomRgbColor", () => {
-  // testando se a função foi chamada.
-  // Não simulamos nenhum comportamento aqui, pois, para esse teste, isso não importa!
-  // Queremos saber se ela foi chamada e ponto final.
-  
-  service.randomRgbColor = jest.fn();
+  // testando quantas vezes a função foi chamada e qual seu retorno
+  service.randomRgbColor = jest
+    .fn()
+    .mockReturnValue('default value')
+    .mockReturnValueOnce('first call')
+    .mockReturnValueOnce('second call');
 
-  service.randomRgbColor();
-  expect(service.randomRgbColor).toHaveBeenCalled();
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(0);
+
+  expect(service.randomRgbColor()).toBe("first call");
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(1);
+
+  expect(service.randomRgbColor()).toBe("second call");
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(2);
+
+  expect(service.randomRgbColor()).toBe("default value");
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(3);
 });
