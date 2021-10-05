451c17d4d42ff60905c6a61caf44d6d35229f43b
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-05 12:39:34 -0300

    Inicia COntent.jsx

--- front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/exercise3-app/src/App.js
+++ front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/exercise3-app/src/App.js
@@ -1,17 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
 import HeaderClass from './Header';
+import ContentClass from './Content';
 
-function testeAlert() {
-  alert("Teste");
-}
+// function testeAlert() {
+//   alert("Teste");
+// }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button type="button" onClick={testeAlert}>Teste</button>
+        {/* <button type="button" onClick={testeAlert}>Teste</button> */}
         <HeaderClass/>
+        <ContentClass/>
       </header>
     </div>
   );
