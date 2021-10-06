5c4937e29cae8526298fcfbf1ed0b83d63138769
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-06 01:50:36 -0300

    Adiciona component teste.

--- front-end/bloco-10-B-introducao-a-react/dia-2-componentes-react/test-app-props/src/App.js
+++ front-end/bloco-10-B-introducao-a-react/dia-2-componentes-react/test-app-props/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Image from './Image';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Image/>
       </header>
     </div>
   );
