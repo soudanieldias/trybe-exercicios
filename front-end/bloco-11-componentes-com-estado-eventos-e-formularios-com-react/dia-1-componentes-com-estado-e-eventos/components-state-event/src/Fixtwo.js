348d722ffd2643a390470426802673d3bf9f7d2f
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-09 20:04:28 -0300

    Adiciona States do button handleClick e resetState.

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/components-state-event/src/Fixtwo.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+class Fixtwo extends React.Component {
+  
+  // Constructor
+  constructor() {
+    super();
+    
+    // console.log("Componente sendo construído. [Fix Two]");
+    
+    this.state = {
+      numberClicks: 0,
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+    this.resetState = this.resetState.bind(this);
+  }
+
+  // Functions
+  handleClick() {
+    // console.log('Clicou no botão!');
+    // console.log(this);
+    // this.setState({
+    //   numberClicks: 1,
+    // });
+    this.setState((prevState, _props) => ({
+      numberClicks: prevState.numberClicks + 1
+    }));
+  }
+  resetState() {
+    this.setState({
+      numberClicks: 0,
+    })
+  }
+  
+  render() {
+    console.log(this);
+    return (
+        <div>
+            <h3>Componentes com Estados e Eventos</h3>
+            <button onClick={this.handleClick}>Botão ClickListener</button>
+            <button onClick={this.resetState}>Reset ClickListener</button>
+            <h1>Número Clicks</h1>
+            <p>{this.state.numberClicks}</p>
+        </div>
+    )
+  }
+}
+
+export default Fixtwo;
