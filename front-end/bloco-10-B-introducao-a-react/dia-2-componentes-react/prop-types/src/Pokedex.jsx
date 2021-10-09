e6c86b49a417dd61a3075ee61ce47c4fb98e3265
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-09 03:07:21 -0300

    Adiciona chamada do Modulo Pokemon na Pokedex.

--- front-end/bloco-10-B-introducao-a-react/dia-2-componentes-react/prop-types/src/Pokedex.jsx
+++ front-end/bloco-10-B-introducao-a-react/dia-2-componentes-react/prop-types/src/Pokedex.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import pokemons from "./Data";
+import Pokemon from "./Pokemon";
 
 class Pokedex extends React.Component {
     render() {
         return (
-            <div>Div que Renderiza os dados dos Pokémons</div>
+            <div>
+                <div>Div que Renderiza os dados dos Pokémons</div>
+                {/* <div>{pokemons}</div> */}
+                <div>
+                    { pokemons
+                    .map( pokemon => {
+                    <Pokemon pokemon={pokemon}/>
+                    }
+
+                )}
+                </div>
+            </div>
         );
     }
 }
