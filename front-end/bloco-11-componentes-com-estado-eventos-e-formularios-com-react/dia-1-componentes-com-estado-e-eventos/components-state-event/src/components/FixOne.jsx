396322882eb6ae92c022c498fb468d4ffd308cbf
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-09 18:43:10 -0300

    Altera arquivos.

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/components-state-event/src/components/FixOne.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Fixone extends React.component {
+    render() {
+        return (
+            <div>
+                <h3>Teste</h3>
+            </div>
+        )
+    }
+}
+
+export default Fixone;
