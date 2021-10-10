5293a7f11885df1504f14c071fcc03049824154c
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-10 03:26:14 -0300

    Finaliza implementação e tratamento de Erros.

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/trybe-color-filter/src/Colorfilter.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/trybe-color-filter/src/Colorfilter.js
@@ -17,25 +17,35 @@ class Colorfilter extends React.Component {
     // console.log(event);
     this.setState({
       typedText: event.target.value,
-      backgroundColor: 'none',
+      bgColor: 'none',
     })
   }
 
+  getFirstColor() {
+    if(!this.state.typedText) return '#303030';
+    const firstColor = colors.find(color => color.name.includes(this.state.typedText));
+    if(!firstColor) return '#303030';
+    return firstColor.hex;
+    // this.setState({
+    //   backgroundColor: firstColor,
+    // })
+  }
   render() {
     return (
       <div className="main">
         <input onChange={this.handleChange} value={this.state.typedText} type="text" />
-        <div style={{backgroundColor: this.state.backgroundColor }} className="virginia">
+        <div style={ { backgroundColor: this.getFirstColor() } } className="virginia">
         <img src={imagem} alt="Fundo Virgínia"></img>
         </div>
         <ul>
-          { colors.filter( color => color.name.includes(this.state.typedText))
-          .map( color =>
-            <li key={color.hex}>
-            <div style={{backgroundColor: color.hex }} className="display-color"></div>
-            { color.name }
-            </li>
-            )}
+          { colors
+            .filter( color => color.name.includes(this.state.typedText))
+            .map( color => (
+              <li key={color.hex}>
+                <div style={{backgroundColor: color.hex }} className="display-color"></div>
+                { color.name }
+              </li>
+          ))}
         </ul>
       </div>
     )}
