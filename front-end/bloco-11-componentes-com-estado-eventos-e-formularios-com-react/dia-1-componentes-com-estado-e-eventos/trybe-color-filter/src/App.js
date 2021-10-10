6e461ccf91a46e36078e47f673401f241349889f
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-10 00:36:15 -0300

    Adiciona componentes do color-filter.

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/trybe-color-filter/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/trybe-color-filter/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import colors from './colors';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </header> */}
+      <input type="text" />
+      <ul>
+        { colors.map( color =>
+        <li key={color.hex}>
+          <div style={{backgroundColor: color.hex }} className="display-color"></div>
+          { color.name }
+        </li>
+        ) }
+      </ul>
     </div>
   );
 }
