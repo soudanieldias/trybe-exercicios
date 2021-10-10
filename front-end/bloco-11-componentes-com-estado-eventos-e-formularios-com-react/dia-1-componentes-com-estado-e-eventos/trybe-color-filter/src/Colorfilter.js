5f5344f6cbec7404196ff3f961c5c2a84948a8ac
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-10 02:08:01 -0300

    Cria funçao que busca a cor de acordo com o estado do objeto.

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/trybe-color-filter/src/Colorfilter.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/trybe-color-filter/src/Colorfilter.js
@@ -5,13 +5,26 @@ class Colorfilter extends React.Component {
   constructor () {
     super();
     
+    this.state = {
+      typedText: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(event) {
+    // console.log(event);
+    this.setState({
+      typedText: event.target.value,
+    })
+  }
+
   render() {
     return (
       <div>
-        <input type="text" />
+        <input onChange={this.handleChange} value={this.state.typedText} type="text" />
         <ul>
-          { colors.map( color =>
+          { colors.filter( color => color.name.includes(this.state.typedText))
+          .map( color =>
             <li key={color.hex}>
             <div style={{backgroundColor: color.hex }} className="display-color"></div>
             { color.name }
