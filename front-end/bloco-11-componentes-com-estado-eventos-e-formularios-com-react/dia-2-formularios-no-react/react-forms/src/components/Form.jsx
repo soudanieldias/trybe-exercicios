aa20d6acb64ca6b3fb893a26a8286330d4d94a34
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-11 23:41:37 -0300

    Adciona fieldset e input para arquivos.

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/react-forms/src/components/Form.jsx
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/react-forms/src/components/Form.jsx
@@ -36,38 +36,50 @@ class Form extends React.Component {
 
         <div className="container">
           <form>
-            <span>Formulário React!</span>
+          <fieldset>
+            <legend>Candidate-se à Vaga</legend>
+            {/* <span>Formulário React!</span> */}
 
             <div>
-            <label htmlFor="fullname">Nome Completo: </label>
-            <input type="text" name="fullname" id="fullname" value={this.state.fullname} onChange={this.handleChange} />
+              <label htmlFor="fullname">Nome Completo</label>
+              <input type="text" name="fullname" id="fullname" autoComplete="off" placeholder="Nome Sobrenome" value={this.state.fullname} onChange={this.handleChange} />
             </div>
 
             <div>
-            <label htmlFor="age">Idade: </label>
-            <input type="number" name="age" id="age" min="13" max="100" value={this.state.age} onChange={this.handleChange} />
+              <label htmlFor="age">Idade</label>
+              <input type="number" name="age" id="age" min="14" max="55" autoComplete="off" placeholder="14" value={this.state.age} onChange={this.handleChange} />
             </div>
 
             <div>
-            {/* <span>Sexo: </span> */}
-            <label htmlFor="gender">Sexo: </label>
-            <select name="gender" value={this.state.gender} onChange={this.handleChange}>
-            <option value="male" selected>Masculino</option>
-            <option value="female">Feminino</option>
-            </select>
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" id="email" placeholder="email@dominio.com"/>
             </div>
 
             <div>
-            <label htmlFor="aboutme">Sobre você: </label>
-            <textarea name="aboutme" id="aboutme" value={this.state.aboutme} onChange={this.handleChange} cols="30" rows="10"></textarea>
+              <label htmlFor="gender">Sexo</label>
+              <select name="gender" value={this.state.gender} onChange={this.handleChange}>
+              <option value="male" selected>Masculino</option>
+              <option value="female">Feminino</option>
+              </select>
             </div>
 
             <div>
-            <label htmlFor="check">Finalizado?</label>
-            <input type="checkbox" name="check" id="check" value={this.state.check} onChange={this.handleChange} />
+              <label htmlFor="aboutme">Sobre você</label>
+              <textarea name="aboutme" id="aboutme" value={this.state.aboutme} onChange={this.handleChange} cols="30" rows="10"></textarea>
             </div>
 
+            <div>
+              <label htmlFor="check">Finalizado?</label>
+              <input type="checkbox" name="check" id="check" value={this.state.check} onChange={this.handleChange} />
+            </div>
+
+            <div>
+              <label htmlFor="file">Envie seu Currículo</label>
+              <input type="file" name="file" id="file"/>
+            </div>
+            
             <input type="submit" value="Enviar" />
+          </fieldset>
           </form>
         </div>
       );
