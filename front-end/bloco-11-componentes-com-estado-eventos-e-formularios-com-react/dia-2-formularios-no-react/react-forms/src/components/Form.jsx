30a3e78bab6f7d1c0ec7c22abbac3d87c5e47636
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-11 22:12:35 -0300

    Adiciona Forms do exercicio.

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/react-forms/src/components/Form.jsx
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/react-forms/src/components/Form.jsx
@@ -1,10 +1,58 @@
 import React from 'react';
+import '../Form.css';
 
-class Form extends React.Components {
+class Form extends React.Component {
+
+  constructor() {
+    super();
+
+    // this.function = this.function.bind(this);
+
+    this.state = {
+      estadoFavorito: '',
+    };
+  }
+
+  // handleChange(event) {
+  //   this.setState({
+  //     estadoFavorito: event.target.value,
+  //   });
+
   // Estrutura do Formulário
   render() {
       return (
-        // Retorno
+
+        <div className="container">
+          <form>
+            <span>Formulário React!</span>
+
+            <div>
+            <label htmlFor="name">Nome Completo: </label>
+            <input type="text" name="name" id="name" />
+            </div>
+
+            <div>
+            <label htmlFor="age">Idade: </label>
+            <input type="number" name="age" id="age" />
+            </div>
+
+            <div>
+            {/* <span>Sexo: </span> */}
+            <label htmlFor="gender">Sexo: </label>
+            <select name="gender">
+            <option value="male" selected>Masculino</option>
+            <option value="Female">Feminino</option>
+            </select>
+            </div>
+
+            <div>
+            <label htmlFor="aboutme">Sobre você: </label>
+            <textarea name="aboutme" id="aboutme" cols="30" rows="10"></textarea>
+            </div>
+
+            <input type="submit" value="Enviar" />
+          </form>
+        </div>
       );
   }
 }
