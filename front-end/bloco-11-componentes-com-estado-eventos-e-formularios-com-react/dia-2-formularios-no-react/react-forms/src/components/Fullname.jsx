52ba4ac9afa1918d6c12cecb8453db24e70d2997
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-12 00:12:58 -0300

    Adiciona componente separado para Fullname.

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/react-forms/src/components/Fullname.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Fullname extends React.Component {
+  render() {
+    const { value, handleChange } = this.props;
+
+    return (
+      <div>
+        <label htmlFor="fullname">Nome Completo</label>
+        <input
+            type="text"
+            name="fullname"
+            id="fullname"
+            autoComplete="off"
+            placeholder="Nome Sobrenome"
+            value={value}
+            onChange={handleChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default Fullname;
