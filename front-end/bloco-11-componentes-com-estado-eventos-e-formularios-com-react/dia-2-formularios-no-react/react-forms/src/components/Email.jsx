64da4474f392286c806151f1589e315b593d1187
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-12 00:26:11 -0300

    Adiciona component Email.

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/react-forms/src/components/Email.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Email extends React.Component {
+  render() {
+      const { value, handleChange } = this.props;
+    return (
+      <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={value}
+            onChange={handleChange}
+            placeholder="email@dominio.com"
+            autoComplete="off"
+          />
+      </div>
+    )
+  }
+}
+
+export default Email;
