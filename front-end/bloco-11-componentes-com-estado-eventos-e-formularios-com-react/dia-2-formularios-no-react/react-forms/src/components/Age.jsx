e5f4fa82bbd2c062224ab13e3fa60aa35dd8208f
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-12 00:26:51 -0300

    Formata estrutura do input Age dentro do codigo.

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/react-forms/src/components/Age.jsx
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/react-forms/src/components/Age.jsx
@@ -7,7 +7,17 @@ class Age extends React.Component {
     return (
         <div>
             <label htmlFor="age">Idade</label>
-            <input type="number" name="age" id="age" min="14" max="55" autoComplete="off" placeholder="14" value={value} onChange={handleChange} />
+            <input
+                type="number"
+                name="age"
+                id="age"
+                min="14"
+                max="55"
+                autoComplete="off"
+                placeholder="14"
+                value={value}
+                onChange={handleChange}
+            />
         </div>
     )
   }
