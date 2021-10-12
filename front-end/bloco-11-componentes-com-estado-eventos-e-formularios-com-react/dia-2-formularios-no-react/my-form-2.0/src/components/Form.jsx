623a1f68ff5252ee70690ae491abda42d2240a41
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-12 18:46:04 -0300

    Adiciona estrutura primaria ao Form.

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/components/Form.jsx
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/components/Form.jsx
@@ -1,10 +1,81 @@
 import React from 'react';
+import '../Form.css';
+import Estados from './Data.json';
 
 class Form extends React.Component {
+  
+  constructor() {
+    super();
+
+    this.state = ({
+      name: '',
+      email: '',
+      cpf: '',
+      age: '',
+      address: '',
+    });
+  }
+
   render() {
+    // EStrutura HTML do Form
     return (
-        <div>
+        <div className="formClass">
             <form>
+              <fieldset>
+                <legend>Dados Pessoais</legend>
+
+                <label htmlFor="name">
+                Nome: <input type="text" name="name" id="name" required />
+                </label>
+              
+                <label htmlFor="email">
+                  Email:
+                  <input type="email" name="email" id="email" required />
+                </label>
+
+                <label htmlFor="cpf">
+                  CPF:
+                  <input type="number" name="cpf" id="cpf" required />
+                </label>
+
+                <label htmlFor="address">
+                  Endereço: 
+                  <input type="text" name="address" id="address" required />
+                </label>
+
+                <label htmlFor="state">
+                  Estado:
+                  <input type="text" name="state" id="state" required />
+                </label>
+
+                <label htmlFor="city">
+                  Cidade: 
+                  <input type="text" name="city" id="city" required />
+                </label>
+
+                <label htmlFor="type">
+                  Tipo:
+                  <input type="text" name="type" id="type" required />
+                </label>
+
+              </fieldset>
+
+              <fieldset>
+                <legend>Dados do Último Emprego</legend>
+
+                <label htmlFor="">
+                  <input type="text" name="" id="" />
+                </label>
+
+                <label htmlFor="">
+                  <input type="text" name="" id="" />
+                </label>
+
+                <label htmlFor="">
+                  <input type="text" name="" id="" />
+                </label>
+              </fieldset>
+
               <button type="submit">Enviar</button>
               <button type="reset">Limpar</button>
             </form>
