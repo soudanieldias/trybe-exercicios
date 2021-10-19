2234e918dc964f488e13ff3f83e4a5cfcbe01815
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-19 01:56:20 -0300

    Incrementa funçoes do Player.

--- front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/lifecycle-app/src/components/SongPlayer.jsx
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/lifecycle-app/src/components/SongPlayer.jsx
@@ -8,20 +8,32 @@ class SongPlayer extends Component {
   state = {
     audio: new Audio(song),
     isPlaying: false,
+    isStoped: true
   };
 
   playPause = () => {
     let isPlaying = this.state.isPlaying;
+    let isStoped = this.state.isStoped;
     if (isPlaying) {
       this.state.audio.pause();
     } else {
       this.state.audio.play();
     }
-    this.setState({ isPlaying: !isPlaying });
+    this.setState({
+      isPlaying: !isPlaying,
+      isStoped: !isStoped,
+    });
   };
+  
+  stopFunc = () => {
+    this.state.audio.pause();
+    this.state.audio.currentTime = 0;
+
+    this.setState({ isPlaying: false });
+  }
 
   render() {
-    const { isPlaying } = this.state;
+    const { isPlaying, isStoped } = this.state;
     const textSong = song.slice(14,30);
     return (
       <div>
@@ -34,6 +46,9 @@ class SongPlayer extends Component {
             {isPlaying ? 
             "Pause" : 
             "Play"}
+        </button>
+        <button disabled={ isStoped } onClick={ (audio) => { this.stopFunc() } }>
+          Stop
         </button>
       </div>
     );
