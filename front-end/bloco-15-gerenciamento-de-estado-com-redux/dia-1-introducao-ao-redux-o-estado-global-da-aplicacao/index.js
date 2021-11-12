202f0771f5e9182cc5d028bc414c5eb5c2f280e6
Daniel Dias <danieldias.b99@hotmail.com>
2021-11-12 14:56:11 -0300

    Corrige indentaçoo do index.

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/index.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/index.js
@@ -2,30 +2,30 @@ const Redux = require('redux');
 
 const fazerLogin = (email) => ({
     type: "LOGIN",
-    email});
+    email
+});
+
+const ESTADO_INICIAL = {
+  login: false,
+  email: "",
+};
+
+const reducer = (state = ESTADO_INICIAL, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        login: !state.login,
+        email: action.email,
+      };
+    default: // No switch, sempre precisamos ter um caso default!
+      return state;
+  }
+};
   
-  const ESTADO_INICIAL = {
-    login: false,
-    email: "",
-  };
+const store = Redux.createStore(reducer);
   
-  const reducer = (state = ESTADO_INICIAL, action) => {
-    switch (action.type) {
-      case "LOGIN":
-        return {
-          ...state,
-          login: !state.login,
-          email: action.email,
-        };
-      default: // No switch, sempre precisamos ter um caso default!
-        return state;
-    }
-  };
+store.dispatch(fazerLogin("alguem@email.com"));
   
-  const store = Redux.createStore(reducer);
-  
-  store.dispatch(fazerLogin("alguem@email.com"));
-  
-  console.log(store.getState());
-  
-  // { login: true, email: 'alguem@email.com' }
+console.log(store.getState());
+// { login: true, email: 'alguem@email.com' }
