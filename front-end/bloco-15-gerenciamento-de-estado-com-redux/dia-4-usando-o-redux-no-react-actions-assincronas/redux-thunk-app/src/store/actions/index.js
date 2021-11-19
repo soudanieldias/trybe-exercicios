af1dbeabe88b33bcfe7f3356095051cf1a4117ac
Daniel Dias <danieldias.b99@hotmail.com>
2021-11-19 12:17:39 -0300

    Captura Payload nas Actions.

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-4-usando-o-redux-no-react-actions-assincronas/redux-thunk-app/src/store/actions/index.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-4-usando-o-redux-no-react-actions-assincronas/redux-thunk-app/src/store/actions/index.js
@@ -2,20 +2,22 @@ export const REQUEST_ISS_LOCATION = 'REQUEST_ISS_LOCATION';
 export const REQUEST_ISS_LOCATION_SUCCESS = 'REQUEST_ISS_LOCATION_SUCCESS';
 export const REQUEST_ISS_LOCATION_ERROR = 'REQUEST_ISS_LOCATION_ERROR';
 
-export function requestISSLocation( ) {
+export function requestISSLocation() {
   return {
     type: REQUEST_ISS_LOCATION,
   };
 }
 
-export function requestISSLocationSuccess( ) {
+export function requestISSLocationSuccess(payload) {
   return {
     type: REQUEST_ISS_LOCATION_SUCCESS,
+    payload,
   };
 }
 
-export function requestISSLocationError( ) {
+export function requestISSLocationError(payload) {
   return {
     type: REQUEST_ISS_LOCATION_ERROR,
+    payload,
   };
 }
