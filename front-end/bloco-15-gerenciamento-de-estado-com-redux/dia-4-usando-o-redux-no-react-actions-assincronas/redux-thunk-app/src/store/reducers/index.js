787fdff84a0a92d50f113f46fc52d0aeb513b3d6
Daniel Dias <danieldias.b99@hotmail.com>
2021-11-19 13:33:22 -0300

    Limpa reducer do Index.

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-4-usando-o-redux-no-react-actions-assincronas/redux-thunk-app/src/store/reducers/index.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-4-usando-o-redux-no-react-actions-assincronas/redux-thunk-app/src/store/reducers/index.js
@@ -1,26 +1,8 @@
 import { combineReducers } from 'redux';
-import { REQUEST_ISS_LOCATION_SUCCESS } from '../actions';
 import coordinatesReducer from './coordinates';
 
-const INITIAL_STATE = {
-  // Add Initial State Here
-  defaultState: 'Default',
-}
-
-const mainReducer = (state = INITIAL_STATE, action) => {
-  // Add Action Switchers Here
-  switch (action.type) {
-    case REQUEST_ISS_LOCATION_SUCCESS:
-      return {
-        ...state,
-        latitude: action.payload.latitude,
-        longitude: action.payload.longitude,
-      };
-    default:
-      return state;
-  }
-};
-
-const rootReducer = combineReducers({ mainReducer, coordinates: coordinatesReducer });
+const rootReducer = combineReducers({
+  coordinates: coordinatesReducer,
+});
 
 export default rootReducer;
