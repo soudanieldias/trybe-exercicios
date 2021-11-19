35a1ab41c94044d907cd87f7e921f2391fb2d8c1
Daniel Dias <danieldias.b99@hotmail.com>
2021-11-19 08:54:02 -0300

    Finaliza configuraçao da Store/Actions/Reducers do REDUX.

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-4-usando-o-redux-no-react-actions-assincronas/redux-thunk/src/store/reducers/index.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-4-usando-o-redux-no-react-actions-assincronas/redux-thunk/src/store/reducers/index.js
@@ -1,18 +1,23 @@
 import { combineReducers } from 'redux';
-
+import { CHANGE_DEFAULT } from '../actions';
 
 const INITIAL_STATE = {
   // Add Initial State Here
   defaultState: 'Default',
 }
 
-const reducer = () => {
+const reducer = (state = INITIAL_STATE, action) => {
   // Add Action Switchers Here
   switch (action.type) {
+    case CHANGE_DEFAULT:
+      return {
+        ...state,
+        defaultState: action.payload
+      };
     default:
       return state;
   }
-}
+};
 
 const rootReducer = combineReducers({reducer});
 
