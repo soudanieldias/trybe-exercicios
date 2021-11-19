5ec73dbcdf611eb3b6bd8c8debc11de58af0b84c
Daniel Dias <danieldias.b99@hotmail.com>
2021-11-19 11:03:50 -0300

    Corrige index da store.

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-4-usando-o-redux-no-react-actions-assincronas/redux-thunk-app/src/store/index.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-4-usando-o-redux-no-react-actions-assincronas/redux-thunk-app/src/store/index.js
@@ -4,8 +4,8 @@ import thunk from 'redux-thunk';
 
 const store = createStore(
     rootReducer,
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)//,
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
