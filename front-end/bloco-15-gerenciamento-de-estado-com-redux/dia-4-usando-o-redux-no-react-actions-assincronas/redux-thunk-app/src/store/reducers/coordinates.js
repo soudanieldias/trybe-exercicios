5167a80effed1c5825c6a1cd9fd8113fcdd8f873
Daniel Dias <danieldias.b99@hotmail.com>
2021-11-19 13:32:31 -0300

    Adiciona action do Reducer coordinates.

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-4-usando-o-redux-no-react-actions-assincronas/redux-thunk-app/src/store/reducers/coordinates.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-4-usando-o-redux-no-react-actions-assincronas/redux-thunk-app/src/store/reducers/coordinates.js
@@ -1,12 +1,20 @@
-const INITIAL_STATE = {
-    latitude: 0,
-    longitude: 0,
-}
+import { REQUEST_ISS_LOCATION_SUCCESS } from '../actions';
 
-const coordinatesReducer = (state = INITIAL_STATE, action) => {
-  switch(action.type) {
-    default:
-      return state;
+const INITIAL_STATE = {
+  latitude: 0,
+  longitude: 0,
+};
+
+function coordinatesReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case REQUEST_ISS_LOCATION_SUCCESS:
+    return {
+      ...state,
+      latitude: action.payload.latitude,
+      longitude: action.payload.longitude,
+    };
+  default:
+    return state;
   }
 }
 
