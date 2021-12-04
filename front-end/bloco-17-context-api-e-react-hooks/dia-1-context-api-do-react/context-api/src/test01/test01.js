84653683c87de214e82f5bf90d656ac8e610265b
Daniel Dias <danieldias.b99@hotmail.com>
2021-12-04 20:20:11 -0300

    Salva alteraçoes de arquivos.

--- front-end/bloco-17-context-api-e-react-hooks/dia-1-context-api-do-react/context-api/src/test01/test01.js
+++ front-end/bloco-17-context-api-e-react-hooks/dia-1-context-api-do-react/context-api/src/test01/test01.js
@@ -3,12 +3,22 @@ import Test01Child from './test01_child';
 import { MoneyProvider } from './moneyContext';
 
 class Test01 extends Component {
-
+  constructor(props) {
+    super(props);
+    
+    
+    this.state = {
+      state01: '01',
+      state02: '02',
+      // money,
+    };
+  }
   // Método Render
   render() {
     const money = { value: 1000000, currency: 'R$' };
+    const testState = this.state;
     return(
-      <MoneyProvider value={money}>
+      <MoneyProvider value={{testState, money}}>
         <div>
           <p>ARQUIVO: Test01</p>
           <Test01Child />
--- front-end/bloco-17-context-api-e-react-hooks/dia-1-context-api-do-react/context-api/src/test01/test01_child.js
+++ front-end/bloco-17-context-api-e-react-hooks/dia-1-context-api-do-react/context-api/src/test01/test01_child.js
@@ -5,22 +5,28 @@ class Test01Child extends Component {
   static contextType = MoneyContext;
 
   componentDidMount() {
-    const money = this.context
+    // const money = this.context;
 
-    console.log(money) // { value: 1000000, currency: 'R$' }
+    // console.log(money) // { value: 1000000, currency: 'R$' }
+    console.log('Context: ');
+    console.log(this.context);
+    console.log(MoneyContext);
   }
 
   render() {
-    const money = this.context;
+    const { money } = this.context;
 
     return (
-      <div>
-        <p>ARQUIVO: Test01_Child</p>
-        { ' ' }
-        { money.currency }
-        { ' ' }
-        { money.value }
-      </div>
+        <div>
+          <p>ARQUIVO: Test01_Child</p>
+          { ' ' }
+          {/* { money['currency'] } */}
+          { money.currency }
+          { ' ' }
+          {/* { money['value'] } */}
+          { money.value }
+          { ' ' }
+        </div>
     )
   }
 }
