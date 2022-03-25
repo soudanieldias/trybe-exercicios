e7cdff9cd75d28d2d56633b6b75ba8abdf6e136f
“soudanieldias” <danieldias.b99@hotmail.com>
2022-03-24 22:40:06 -0300

    Adiciona primeiros arquivos TS

---
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/exercicios/fixacao01.ts
@@ -0,0 +1,18 @@
+/*
+ * Crie uma Enum que represente os dias da semana . Seu valor deve ser número do dia.
+ * Crie uma Enum que represente as cores do arco iris . Seu valor deve ser o nome das cores em português.
+ * Crie uma Enum que represente as ações: salvar , imprimir , abrir , visualizar e fechar . Seu valor deve ser do tipo inteiro.
+ * Crie uma Enum que represente os pontos cardeais: Norte , Leste , Sul e Oeste . Seu valor deve ser a primeira letra do nome do ponto cardial.
+ */
+
+enum diasDaSemana {
+  segunda,
+  terca,
+  quarta,
+  quinta,
+  sexta,
+  sabado,
+  domingo,
+}
+
+console.log(diasDaSemana);
