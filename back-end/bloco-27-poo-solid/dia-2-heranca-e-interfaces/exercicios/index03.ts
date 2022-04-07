8893e56b74250bc80b11707b4491ce38e8acb1d7
“soudanieldias” <danieldias.b99@hotmail.com>
2022-04-07 13:02:12 -0300

    Adiciona exercício 03

---
+++ back-end/bloco-27-poo-solid/dia-2-heranca-e-interfaces/exercicios/index03.ts
@@ -0,0 +1,17 @@
+// Para testar!
+// ./index.ts
+
+import Employee from './exercicio03';
+
+const testInterfaceEmployee: Employee = {
+    registration: 'FNC1234567891011',
+    salary: 1200.00,
+    admissionDate: new Date(),
+    generateRegistration(): string {
+        const randomStr = String(Date.now() * (Math.random() + 1)).replace(/\W/g, '');
+
+        return `FNC${randomStr}`;
+    }
+}
+
+console.log(testInterfaceEmployee);
