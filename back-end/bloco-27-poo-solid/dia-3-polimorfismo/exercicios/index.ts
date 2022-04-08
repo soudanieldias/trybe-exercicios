ad290b10a123c36e0c3a7a76065d32f0fb763bab
“soudanieldias” <danieldias.b99@hotmail.com>
2022-04-08 15:27:52 -0300

    Adiciona alterações nos Arquivos

--- back-end/bloco-27-poo-solid/dia-3-polimorfismo/exercicios/index.ts
+++ back-end/bloco-27-poo-solid/dia-3-polimorfismo/exercicios/index.ts
@@ -1,21 +1,37 @@
 // ./index.ts
 
-import Person from './Person';
+// import Person from './Person';
 import Teacher from './Teacher';
 import Student from './Student';
 import Subject from './Subject';
-
-// ./index.ts
+// import Exam from './Exam';
+// import Work from './Work';
 
 const carolina = new Student('Carolina da Silva', new Date('2005/03/17'));
 const lucas = new Student('Lucas Peixoto Salgueiro', new Date('2006/07/19'));
-console.log(carolina);
-console.log(lucas);
 
 const math = new Subject('Matemática');
 const history = new Subject('História');
 
 const marta = new Teacher('Marta da Silva', new Date('1980/03/30'), 2000, math);
 const joao = new Teacher('João Antônio da Costa', new Date('1982/04/21'), 2000, history);
-console.log(marta);
-console.log(joao);
+
+// const sandwiche = new OrderItem('Sandwiche Natural', 5.00);
+// const juice = new OrderItem('Suco de Abacaxí', 5.00);
+// const dessert = new OrderItem('Gelatina de Uva', 2.50);
+
+// const carolinaOrder = new Order(carolina, [sandwiche, juice, dessert], 'dinheiro', 0.10);
+// const lucasOrder = new Order(lucas, [sandwiche, juice], 'dinheiro', 0.10);
+// const martaOrder = new Order(marta, [sandwiche, juice], 'cartão');
+// const joaoOrder = new Order(joao, [sandwiche, juice, dessert], 'cartão');
+
+// const orderRepository = new OrderRepository();
+// orderRepository.addOrder(carolinaOrder);
+// orderRepository.addOrder(lucasOrder);
+// orderRepository.addOrder(martaOrder);
+// orderRepository.addOrder(joaoOrder);
+// orderRepository.addOrder(carolinaOrder);
+
+// console.log('Pedidos da Carolina: ', orderRepository.listByClient(carolina));
+// console.log('Maior valor para o menor: ', orderRepository.listBySortedValue('maior'));
+// console.log('Menor valor para o maior: ', orderRepository.listBySortedValue('menor'));
