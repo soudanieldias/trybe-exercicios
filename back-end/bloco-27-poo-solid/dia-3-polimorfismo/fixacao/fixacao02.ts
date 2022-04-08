fdd4a2340d2cc85faf724070eb9ee21080b3483a
“soudanieldias” <danieldias.b99@hotmail.com>
2022-04-08 02:13:40 -0300

    Conclui fixação

--- back-end/bloco-27-poo-solid/dia-3-polimorfismo/fixacao/fixacao02.ts
+++ back-end/bloco-27-poo-solid/dia-3-polimorfismo/fixacao/fixacao02.ts
@@ -71,3 +71,39 @@ class CharacterService {
 
   /* Implementação dos outros métodos */
 }
+
+// Crie uma classe MockedDbModel que implemente IModel com uma versão mock.
+class MockDbModel implements IModel {
+  async create(character: Character) {
+    console.log('character created');
+    return { id: 1, name: 'Peach', specialMove: 'Toad' };
+  };
+
+  async update(id: number, character: Character) {
+    console.log('character updated');
+    return { id: 1, name: 'Yoshi', specialMove: 'Egg Lay' };
+  };
+
+  async delete(id: number) {
+    console.log('character deleted');
+    return true;
+  };
+
+  async getAll() {
+    return [
+      { id: 1, name: 'Samus', specialMove: 'Charge Shot' },
+      { id: 2, name: 'Kirby', specialMove: 'Inhale' }
+    ];
+  }
+
+  async getById(id: number) {
+    return { id: 1, name: 'Mario', specialMove: 'Fireball' };
+  }
+}
+
+// Verifique que a classe CharacterService pode receber uma instância tanto de LocalDbModel como de MockedDbModel
+const A = new CharacterService(new LocalDbModel());
+A.getAll().then(console.log);
+
+const B = new CharacterService(new MockDbModel());
+B.getAll().then(console.log);
