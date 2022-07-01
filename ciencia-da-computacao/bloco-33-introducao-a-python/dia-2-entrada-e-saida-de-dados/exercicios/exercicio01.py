207204464f6b37d4849a11f582cf66133a8a45d7
“soudanieldias” <danieldias.b99@hotmail.com>
2022-06-30 21:47:43 -0300

    feat: add exercises

---
+++ ciencia-da-computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/exercicios/exercicio01.py
@@ -0,0 +1,11 @@
+# Exercício 1: Faça um programa que receba um nome e imprima o mesmo na vertical em escada invertida.
+
+def vertical_inverted_ladder(word):
+    for removed_letters in range(len(word)):
+        for index in range(len(word) - removed_letters):
+            print(word[index], end="")
+        print()
+
+if __name__ == "__main__":
+    name = input("Digite um nome: ")
+    vertical_inverted_ladder(name)
